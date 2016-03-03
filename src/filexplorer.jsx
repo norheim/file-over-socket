1360c9656f73add80f0e4496973611e30d574852
Johannes Norheim <johannes.j.norheim@gmail.com>
2016-03-03 17:37:24 -0500

    created readFile function
    importing from carbide changes done for easy integratable component

--- src/filexplorer.jsx
+++ src/filexplorer.jsx
@@ -9,38 +9,42 @@ class FileExplorer extends React.Component {
           filenames : []
         };
     this.handleChange = this.handleChange.bind(this);
+    this.readFiles = this.readFiles.bind(this);
 
     // List all files in directory
+    console.log(this.props.socket);
+    this.props.socket.on('connect', function(){console.log('connect')});
     this.props.socket.on('server:listDirResponse'+this.props.folder, msg => {
+      console.log(msg);
       this.setState({filenames: msg});
     });
     this.props.socket.emit('client:listDirRequest', this.props.folder);
-
     // Receive data from server
-    this.props.socket.on('server:readFileResponse'+this.state.selectedFile, msg => {
-      this.props.func(msg);
-    });
+    this.readFiles(this.state.selectedfile);
   }
 
-	handleChange (event) {
-    const filename = event.target.value;
-    
+  readFiles (filename){
     if(filename != "default"){
       this.props.socket.on('server:readFileResponse'+this.state.selectedFile, msg => {});
       this.props.socket.on('server:readFileResponse'+filename, msg => {
-        this.props.func(msg);
+        this.props.func(msg, filename);
       });
-    	this.setState({selectedfile: filename});
+      this.setState({selectedfile: filename});
       const fileRequestData = {
         folder:this.props.folder, 
         filename:filename
       };
       this.props.socket.emit('client:readFileRequest', JSON.stringify(fileRequestData));
     }
+  }
+
+	handleChange (event) {
+    const filename = event.target.value;
+    this.readFiles(filename);
 	}
 
 	render() {
-      let rows = [<option value='default'>Choose a file</option>];
+      let rows = [<option value='default' key='default'>Choose a file</option>];
       const filenames = this.state.filenames;
       for (var i=0; i < filenames.length; i++) {
           rows.push(<option value={filenames[i]} key={filenames[i]}>{filenames[i]}</option>);
